eab803345f77a84125a1b588a32d0bdb94c76401
Ivan Khvostishkov <87804596+ivan-khvostishkov@users.noreply.github.com>
2025-09-08 11:45:01 +0200

    Oxford filter - removing uncertain words

--- sort-traits.sh
+++ sort-traits.sh
@@ -1,1 +1,2 @@
-sort oxford-traits.txt | egrep -v 'though '\|'can '\|'while '\|'when '\|'but ' | cut -d ' ' -f1 | sort | uniq -c | sort -n -r | less
+sort oxford-traits.txt | egrep -v 'though '\|'can '\|'while '\|'when '\|'but ' | cut -d ' ' -f1 | \
+  sort | uniq -c | sort -n -r | tee oxford-traits-sorted.txt | less
